5fe8046a961d84e7512f560694f1fb7e43b372ee
Muhammad Guntur <itawanri13@gmail.com>
2026-03-13 04:58:40 +0700

    Update kamisu_sign.h

--- kernel/kamisu_sign.h
+++ kernel/kamisu_sign.h
@@ -14,6 +14,6 @@
 // kaminarich/KamiSU
 #define EXPECTED_SIZE_KAMISU 0x2e8
 #define EXPECTED_HASH_KAMISU \
-    "31c1a4fe9b7ba64858c2216db969ac47433728550fca966bfeb639b2d1e909ec"
+    "590bdbbdd0e7f9f137da28b9fa99173259cfc81d4ad1601046205c5ca805835c"
 
 #endif /* __KSU_H_KAMISU_SIGN */
